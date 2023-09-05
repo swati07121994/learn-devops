f1b6adb24413d4de5369964fbf1e4d64607760a7
Swati Dhurandhar <swati@Swatis-MacBook-Air.local>
2023-09-05 15:41:24 +0530

    Changes3

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
-// import cockatiel from './cockatiel.png';
+import cockatiel from './images/cockatiel.png';
 
 function App() {
   return (
     <div className="App">
       <h1>Old Cockatiels</h1>
-      {/* <img src={cockatiel} width="500" alt="cuku" /> */}
+      <img src={cockatiel} width="500" alt="cuku" />
     </div>
   );
 }
